67c0cb7dcb4f30a2be0728311fc4ccc49e095634
NaeliScheffler <85200471+NaeliScheffler@users.noreply.github.com>
2021-06-09 15:34:21 -0300

    trabalhando

---
+++ src/ex_semana4/ex_1.java
@@ -0,0 +1,29 @@
+package ex_semana4;
+import java.util.Scanner;
+public class ex_1 {
+    public static void main(String[] args) {
+
+        Scanner texto = new Scanner(System.in);
+        String[] a;
+        int j;
+        System.out.print("Entre com uma frase: ");
+        String frase = texto.nextLine();
+
+        a = frase.split(" ");
+        j=(a.length);
+        j=j-1;
+        for(int i=0;i<a.length;i++){
+            System.out.println(a[j]);
+            j--;
+        }
+
+
+
+
+
+        texto.close();
+
+
+    }
+}
+
