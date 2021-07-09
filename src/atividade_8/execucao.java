2ff2b28840f253821217ad2559f6017290de46f7
NaeliScheffler <85200471+NaeliScheffler@users.noreply.github.com>
2021-07-08 23:17:00 -0300

    trabalhando

--- src/atividade_8/execucao.java
+++ src/atividade_8/execucao.java
@@ -6,7 +6,7 @@ public class execucao {
         funcionarios f1 = new funcionarios();
         f1.nome = "Hugo";
         f1.salario = 100;
-        String data = "";
+        String data;
         data="08/02/2001";
         f1.data_entr=data;
         f1.aumento(100);
