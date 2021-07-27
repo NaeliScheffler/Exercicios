47e81b624b39890bac2772b11ad8bf1b41afde1a
NaeliScheffler <85200471+NaeliScheffler@users.noreply.github.com>
2021-07-26 21:23:43 -0300

    trabalhando

--- src/atividade_9/continente.java
+++ src/atividade_9/continente.java
@@ -1,44 +1,119 @@
 package atividade_9;
 import java.util.ArrayList;
 class Continente {
-    public String nome;
-    public ArrayList<pais> listaPais = new ArrayList<>();
+    private String nome;
+    public ArrayList < pais > listaPais = new ArrayList<>();
 
-    public Continente(){}
-    public Continente(String nome) {
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
         this.nome = nome;
+    }
 
+    public ArrayList < pais > getPaises() {
+        return listaPais;
     }
-    public void adicionaPais(pais Pais) {
-        listaPais.add(Pais);
-    }
-    public double getDimensaoTotal() {
-        double total = 0;
-        for(pais p : listaPais) {
-            total += p.getDimensao();
-        }
-        return total;
-    }
-    public double getPopulacaoTotal() {
-        double total = 0;
 
-        for(pais p : listaPais) {
-            total += p.getPop();
-        }
-        return total;
+    public void setPaises(ArrayList < pais > listaPais) {
+        this.listaPais = listaPais;
     }
-    public String maiorPop(){
-        double total=0;
-        double maiorPop=0;
-        String maior="";
-        for (pais p : listaPais){
-            total=p.getPop();
-            if(maiorPop<=total){
-                maiorPop=total;
+    public void dimensao() {
+        double dimensaoTotal = 0;
+        for (pais p: listaPais) {
+            dimensaoTotal += p.getDimensao();
+        }
+        System.out.println(" Dimensao total do continente: " + dimensaoTotal);
+    }
+
+    public void populacao() {
+        int popTotal = 0;
+        for (pais p: listaPais) {
+            popTotal += p.getPop();
+        }
+        System.out.println(" População total do continente: " + popTotal);
+    }
+
+    public void densidadeP() {
+        int popTotal = 0;
+        double dimensaoTotal = 0;
+        for (pais p: listaPais) {
+            popTotal += p.getPop();
+            dimensaoTotal += p.getDimensao();
+        }
+        System.out.println(" Densidade populacional: " + popTotal / dimensaoTotal);
+    }
+
+    public void maiorPop() {
+        int i = 0;
+        int j = 0;
+        int pop = 0;
+        for (; i < this.listaPais.size(); i++) {
+            if (this.listaPais.get(i).getPop() > pop) {
+                j = i;
+                pop = this.listaPais.get(i).getPop();
             }
-            maior=p.getNome();
         }
-        return maior;
+        System.out.println(" Pais que possui a  maior população: " + this.listaPais.get(j).getNome() + " Possui: " + this.listaPais.get(j).getPop());
+    }
+
+    public void menorPop() {
+        int i = 0;
+        int j = 0;
+        int pop = 0;
+        for (; i < this.listaPais.size(); i++) {
+            if (this.listaPais.get(i).getPop() < pop || i == 0) {
+                j = i;
+                pop = this.listaPais.get(i).getPop();
+            }
+        }
+        System.out.println(" Pais que possui menor população: " + this.listaPais.get(j).getNome() + " Possui: " + this.listaPais.get(j).getPop());
+    }
+
+    public void maiorDimensao() {
+        int i = 0;
+        int j = 0;
+        double dimensao = 0;
+        for (; i < this.listaPais.size(); i++) {
+            if (this.listaPais.get(i).getDimensao() > dimensao) {
+                j = i;
+                dimensao = this.listaPais.get(i).getDimensao();
+            }
+        }
+        System.out.println(" Pais com a maior dimensao: " + this.listaPais.get(j).getNome() + " Possui: " + this.listaPais.get(j).getDimensao());
+    }
+
+    public void menorDimensao() {
+        int i = 0;
+        int j = 0;
+        double dimensao = 0;
+        for (; i < this.listaPais.size(); i++) {
+            if (this.listaPais.get(i).getDimensao() < dimensao || i == 0) {
+                j = i;
+                dimensao = this.listaPais.get(i).getDimensao();
+            }
+        }
+        System.out.println(" Pais com a menor dimensao: " + this.listaPais.get(j).getNome() + " Possui " + this.listaPais.get(j).getDimensao());
+    }
+
+    public void razaoTerritorial() {
+        int maior = 0;
+        int menor = 0;
+        double maiorDimensao = 0;
+        double menorDimensao = 0;
+        for (int i = 0; i < this.listaPais.size(); i++) {
+            if (this.listaPais.get(i).getDimensao() < menorDimensao || i == 0) {
+                menor = i;
+                menorDimensao = this.listaPais.get(i).getDimensao();
+            }
+            if (this.listaPais.get(i).getDimensao() > maiorDimensao || i == 0) {
+                maior = i;
+                maiorDimensao = this.listaPais.get(i).getDimensao();
+            }
+        }
+        double x = this.listaPais.get(maior).getDimensao() / this.listaPais.get(menor).getDimensao();
+        System.out.println(" A diferença entre os territorios eh: " + this.listaPais.get(maior).getNome() + " para " + this.listaPais.get(menor).getNome() + " : " + x);
     }
 
 }
--- src/atividade_9/execucao_2.java
+++ src/atividade_9/execucao_2.java
@@ -1,13 +1,32 @@
 package atividade_9;
 
+import java.util.ArrayList;
+
 public class execucao_2 {
     public static void main(String[] args) {
-
-      Continente c1=new Continente("America");
-      c1.adicionaPais("Brasil");
-
-
-
-
+            Continente c1 =  new Continente ();
+            c1 . setNome ( " América " );
+            pais p1 =  new pais (" BRA " , " BRASIL " , 8.516000 );
+            pais p2 =  new  pais ( " URU " , " URUGUAI " , 176.215);
+            pais p3 =  new  pais ( " BOL " , " BOLÍVIA " , 1.099000 );
+            ArrayList< pais > listaPais =  new  ArrayList < pais > ();
+            listaPais. add (p1);
+            listaPais. add (p2);
+            listaPais. add (p3);
+            c1 .dimensao ();
+            c1 . maiorDimensao ();
+            c1 . menorDimensao ();
+            c1 . populacao ();
+            c1 . maiorPop ();
+            c1 . menorPop ();
+            c1 . setPaises (listaPais);
+            c1 . densidadeP ();
+            c1 . razaoTerritorial ();
+        }
     }
-}
+
+
+
+
+
+
